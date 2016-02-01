630029ccf3ac49d9d46b074b5bdb0e9e8df86e80
mengxiaodi <1104252528@qq.com>
2016-02-01 12:13:13 +0800

    Update YH_JS.js

--- YH_JS.js
+++ YH_JS.js
@@ -20,3 +20,26 @@ var YH_JS={
 		return function(e){fn.call(obj,e)}
 	}
 }
+//小数点后保留几位（四舍五入）
+Number.prototype.toFixedNum = function(s){
+	changenum=(parseInt(this * Math.pow( 10, s ) + 0.5)/ Math.pow( 10, s )).toString();
+	index=changenum.indexOf(".");
+	//index为小数点出现的位置,s为保留几位
+	
+	//如果不存在小数点 56
+	if(index<0&&s>0){
+		changenum=changenum+".";
+		for(i=0;i<s;i++){
+			changenum=changenum+"0";
+		}
+	}else {
+		//如果存在小数点
+		index=changenum.length-index;
+		//console.log("|"+(index));
+		for(i=0;i<=(s-index);i++){
+			changenum=changenum+"0";
+		}
+	}
+	return changenum;
+} 
+
